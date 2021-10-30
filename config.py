0bf06b552058fa8f72d4c903f72f2fdbbd1725a9
yeozhenhao <elgeneyeo@hotmail.com>
2021-10-30 15:17:35 +0800

    singlebot

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ PLAYERS_FILENAME = os.environ['PLAYERS_FILENAME']
 CHAT_ID_JSON = os.environ['CHAT_ID_JSON']
 ANGEL_ALIAS = os.environ['ANGEL_ALIAS']
 MORTAL_ALIAS = os.environ['MORTAL_ALIAS']
+
+'''
+only important for singlebot
+'''
