7fc2f00275ca407cc960249b804601ff634bdc40
yeozhenhao <elgeneyeo@hotmail.com>
2021-10-31 19:45:15 +0800

    Updated for heroku webhook

--- configdualbot.py
+++ configdualbot.py
@@ -7,5 +7,5 @@ CHAT_ID_JSON = os.environ['CHAT_ID_JSON']
 ANGEL_ALIAS = os.environ['ANGEL_ALIAS']
 MORTAL_ALIAS = os.environ['MORTAL_ALIAS']
 herokuappname = os.environ['herokuappname']  ##for bot to listen to heroku Webhook
-ANGEL_BOT_ID = os.environ['ANGEL_BOT_ID'] ##not used atm, was for send_polls
-MORTAL_BOT_ID = os.environ['MORTAL_BOT_ID'] ##not used atm, was for send_polls
+# ANGEL_BOT_ID = os.environ['ANGEL_BOT_ID'] ##not used atm, was for send_polls
+# MORTAL_BOT_ID = os.environ['MORTAL_BOT_ID'] ##not used atm, was for send_polls
