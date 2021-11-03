73cbdd7c6f00bc3a194a6ee2d3ba80fbbf7ef5d4
yeozhenhao <elgeneyeo@hotmail.com>
2021-11-04 01:03:47 +0800

    Fully working dualbot + heroku + PostgreSQL v1 with Gamemaster function

--- dualbotPostgreSQL2.py
+++ dualbotPostgreSQL2.py
@@ -123,13 +123,12 @@ def help_command_MORTAL(update: Update, context: CallbackContext) -> None:
 @player.restricted
 def reload_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /reloadchatids is issued."""
+    PostgreSQLconnect.create_sql_players()
     PostgreSQLconnect.import_players_from_csv()
     logger.info(f'Player data has been imported from local csv into SQL server')
     PostgreSQLconnect.import_playerchatids_fromJSON_toSQL()
     logger.info(f'Player chat ids have been imported from local JSON into SQL server')
-    PostgreSQLconnect.loadPlayers_fromSQL()
-    logger.info(f'Player data & chat ids have been loaded into Telegram dualbot!')
-    update.message.reply_text(f'Players reloaded!')
+    update.message.reply_text(f'Players reloaded into SQL!')
 
 @player.restricted
 def savechatids_command(update: Update, context: CallbackContext) -> None:
