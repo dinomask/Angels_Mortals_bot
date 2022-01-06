375ee2fd6f605c3ad9c86e52a27aeb627015f930
yeozhenhao <elgeneyeo@hotmail.com>
2022-01-07 00:37:28 +0800

    Minor fix: 1. Fixed reply function for symbols '—', '™', '˜', '€'

--- messagesdualbot.py
+++ messagesdualbot.py
@@ -57,10 +57,10 @@ def getNotRegisteredLog(alias, sender, receiver):
 def getMessageEntitybyYou(UpdateMessageText: str, UpdateReplyToMessageText: str, UpdateMessageEntities):
     UpdateMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateMessageText, 'unicode-escape'))
     x1 = sum(map(UpdateMessageText_toUnicodeStr.count, ['\\']))
-    y1 = sum(map(UpdateMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y1 = sum(map(UpdateMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     UpdateReplyToMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateReplyToMessageText, 'unicode-escape'))
     x2 = sum(map(UpdateReplyToMessageText_toUnicodeStr.count, ['\\']))
-    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     effective_message_max_offset_length = len(UpdateMessageText) + x1 - y1
     max_length_reply_message = len(UpdateReplyToMessageText) + x2 - y2
     # print(UpdateMessageText_toUnicodeStr)
@@ -92,10 +92,10 @@ def getMessageEntitybyYou(UpdateMessageText: str, UpdateReplyToMessageText: str,
 def getMessageEntitybyYourALIAS(UpdateMessageText: str, UpdateReplyToMessageText: str, UpdateMessageEntities, LenALIAS):
     UpdateMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateMessageText, 'unicode-escape'))
     x1 = sum(map(UpdateMessageText_toUnicodeStr.count, ['\\']))
-    y1 = sum(map(UpdateMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y1 = sum(map(UpdateMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     UpdateReplyToMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateReplyToMessageText, 'unicode-escape'))
     x2 = sum(map(UpdateReplyToMessageText_toUnicodeStr.count, ['\\']))
-    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     effective_message_max_offset_length = len(UpdateMessageText) + x1 - y1
     max_length_reply_message = len(UpdateReplyToMessageText) + x2 - y2
     print(UpdateMessageText_toUnicodeStr)
@@ -129,7 +129,7 @@ def getMessageEntitybyYourALIAS(UpdateMessageText: str, UpdateReplyToMessageText
 def getMessageEntitybyYou_NoText(UpdateReplyToMessageText: str, UpdateMessageEntities):
     UpdateReplyToMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateReplyToMessageText, 'unicode-escape'))
     x2 = sum(map(UpdateReplyToMessageText_toUnicodeStr.count, ['\\']))
-    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     max_length_reply_message = len(UpdateReplyToMessageText) + x2 - y2
     # print(max_length_reply_message)
     offset_length_before_reply_message = 26  ##26 is the number of char in bot.send_message with reply, "You" template
@@ -155,7 +155,7 @@ def getMessageEntitybyYou_NoText(UpdateReplyToMessageText: str, UpdateMessageEnt
 def getMessageEntitybyYourALIAS_NoText(UpdateReplyToMessageText: str, UpdateMessageEntities, LenALIAS):
     UpdateReplyToMessageText_toUnicodeStr = bytes.decode(str.encode(UpdateReplyToMessageText, 'unicode-escape'))
     x2 = sum(map(UpdateReplyToMessageText_toUnicodeStr.count, ['\\']))
-    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n']))
+    y2 = sum(map(UpdateReplyToMessageText.count, ['…', '’', '‘', '“', '”', '\n', '—', '™', '˜', '€']))
     max_length_reply_message = len(UpdateReplyToMessageText) + x2 - y2
     # print(max_length_reply_message)
     offset_length_before_reply_message = 28 + LenALIAS  ##28 + LenALIAS is the number of char in bot.send_message with reply, "You" template
